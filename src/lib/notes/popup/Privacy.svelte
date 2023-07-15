9925a2cb3367e120aba9df5453823a84fa276da7
Brandooo! <82909107+cptnbrando@users.noreply.github.com>
2023-07-14 20:09:40 -0500

    finaldraft

--- src/lib/notes/popup/Privacy.svelte
+++ src/lib/notes/popup/Privacy.svelte
@@ -32,6 +32,8 @@
        <p>nobody gives a fuck either way. about you, about me, certainly not about these notes</p>
        <p>unless you do give a fuck about these notes</p>
        <p>in which case, like if these past few sentences have already made you feel uneasy, please go away. there's nothing further down here for you.</p>
+       <p>regardless, no. I don't give anybody, or anything, permission to view, read, download, extract, plagiarize, eat, consume, imagine, look at, breath at, listen to, preach, abstract, contemplate, annunciate, or believe anything written on this page, especially anything in my books. Which, of course, are most definately purely fictional I pinkie swear. Do not make this news. Do not make this a social media thing. Don't share it, speak it, think it, and definately do not do anything you read about here. Or else.</p>
+       <p>That goes for anything you've already seen, and anything you see next. This is mine, not yours, leave it for me.</p>
     </div>
     {:else}
         <button on:click={() => {pp = true}}>Click here to read the Privacy Policy</button>
