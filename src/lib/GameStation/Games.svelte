8192f8854daaebe75871abf55d6a871d07b954ba
Brandooo! <becruz1738@gmail.com>
2023-07-16 21:46:25 -0500

    googlesaysnowayjose

--- src/lib/GameStation/Games.svelte
+++ src/lib/GameStation/Games.svelte
@@ -2,10 +2,14 @@
     import Emu from "./Emu.svelte";
 
     EJS_player = '#game';
-    EJS_biosUrl = '/scph1001.bin'; // Url to Bios file
-    // gameurl
-    EJS_gameUrl = `https://doc-00-6g-docs.googleusercontent.com/docs/securesc/4789avb5pb4p6s2gtvm7hgf4g1q8j0kv/5j89525m00denskns5a2vg0im5bhcv08/1689561750000/13623365757509044785/13623365757509044785/1hUKuwGDYxrmc5aYLhlSj9PGJ1mSicCIe?e=download&ax=AGtFMPW-sg3JjeN1Rttdk-6utfKybEkEgPxR2V2ACvolKiTOgAqoH2oLmwIPn9_Fzek6BXvJY5P49fV9szZNfkxMA_W0kJ5McydKk6uJmt9e1wVgDA8cu_sOqnS2zq0T4Uvb70nM9xuWpHI98Fzpxgk0u-OIwW7mVCCIjkt1InYgmGw_aiF1fgsNK0LcQQWMcLJpawWfW1ICgrDPvWmK7oleiuoRoyJFNuP-4FVUuYKt1rzKyu6pvp1dwy1rz1G0-g688NDPEeFltPolwCwK-t-Sv5fySgI3L1TfqaK6i-IyudyA3Kf032pZUV_iNjb1rEdRmpo7IjzL01kh8K6Q2hkj021KOiu3jZKftSaxgYL0vT8chvnE2QzuL_OWxgGlsKVlJXydOjBh5oH9CnehhsMAskOEM2BILfQjetVzwKxHqOPi6InMGBdR_VljUpdCQ-QL1GmHeUraVo-S7eMKH-9GNlCNMjJnifhflSbHNFK-F3se1iHwZob5YKnt0h2PIB7P61AoUZmvRf_IFdnj_5VzqZgDWUy76cgmJgSVmDJNj9MIgvfg14OFzfRc0-ObpI8uIvXDsSyI-OJH-sjVWpOnqY7Og0hijz-cmed-N_XqZrs8pFOzAV7mEyVTtri0e_msJxnnQP0hQFCcr3_o1xL0RTgt3jzg3OAkiOh-HX_zqiveWcnFV0BOtZMgP_lFGcend1XhxNtHQqpyU_XzB4Z5kdOdnka-B2bPLERTxRB4BZIIF-t8kuKJTCFb8DLR9pSccMyhJYPe2FxCiqPJiKFh1zcqUJQMaDjNOxkO_wcNaiBuSyEzYXdHMBx1vMm7bNHhGVNVYdBAjotgD2G_5L_VJr8oa4jw9tmMGVAxKNXMWQ1lXnvDjmqah_8gPupCtzJVRj1OnYV8o7_LTGqmljOAAxj0JAA3o--P99p826U&uuid=71348fec-d4cc-43d9-af52-37b70a51feba&authuser=0&nonce=7b755jahjgi02&user=13623365757509044785&hash=na53nrdba4q98ma09pkod31ah3u0dnih`;
+
+    // Url to Bios file
+    EJS_biosUrl = '/scph1001.bin';
+    EJS_gameUrl = ``;
     EJS_core = 'psx';
+
+    // gameurl
+    // EJS_gameUrl = `https://doc-00-6g-docs.googleusercontent.com/docs/securesc/4789avb5pb4p6s2gtvm7hgf4g1q8j0kv/5j89525m00denskns5a2vg0im5bhcv08/1689561750000/13623365757509044785/13623365757509044785/1hUKuwGDYxrmc5aYLhlSj9PGJ1mSicCIe?e=download&ax=AGtFMPW-sg3JjeN1Rttdk-6utfKybEkEgPxR2V2ACvolKiTOgAqoH2oLmwIPn9_Fzek6BXvJY5P49fV9szZNfkxMA_W0kJ5McydKk6uJmt9e1wVgDA8cu_sOqnS2zq0T4Uvb70nM9xuWpHI98Fzpxgk0u-OIwW7mVCCIjkt1InYgmGw_aiF1fgsNK0LcQQWMcLJpawWfW1ICgrDPvWmK7oleiuoRoyJFNuP-4FVUuYKt1rzKyu6pvp1dwy1rz1G0-g688NDPEeFltPolwCwK-t-Sv5fySgI3L1TfqaK6i-IyudyA3Kf032pZUV_iNjb1rEdRmpo7IjzL01kh8K6Q2hkj021KOiu3jZKftSaxgYL0vT8chvnE2QzuL_OWxgGlsKVlJXydOjBh5oH9CnehhsMAskOEM2BILfQjetVzwKxHqOPi6InMGBdR_VljUpdCQ-QL1GmHeUraVo-S7eMKH-9GNlCNMjJnifhflSbHNFK-F3se1iHwZob5YKnt0h2PIB7P61AoUZmvRf_IFdnj_5VzqZgDWUy76cgmJgSVmDJNj9MIgvfg14OFzfRc0-ObpI8uIvXDsSyI-OJH-sjVWpOnqY7Og0hijz-cmed-N_XqZrs8pFOzAV7mEyVTtri0e_msJxnnQP0hQFCcr3_o1xL0RTgt3jzg3OAkiOh-HX_zqiveWcnFV0BOtZMgP_lFGcend1XhxNtHQqpyU_XzB4Z5kdOdnka-B2bPLERTxRB4BZIIF-t8kuKJTCFb8DLR9pSccMyhJYPe2FxCiqPJiKFh1zcqUJQMaDjNOxkO_wcNaiBuSyEzYXdHMBx1vMm7bNHhGVNVYdBAjotgD2G_5L_VJr8oa4jw9tmMGVAxKNXMWQ1lXnvDjmqah_8gPupCtzJVRj1OnYV8o7_LTGqmljOAAxj0JAA3o--P99p826U&uuid=71348fec-d4cc-43d9-af52-37b70a51feba&authuser=0&nonce=7b755jahjgi02&user=13623365757509044785&hash=na53nrdba4q98ma09pkod31ah3u0dnih`;
 
     // https://drive.google.com/uc?export=download&id=1hUKuwGDYxrmc5aYLhlSj9PGJ1mSicCIe&confirm=t&uuid=71348fec-d4cc-43d9-af52-37b70a51feba
     // ???
