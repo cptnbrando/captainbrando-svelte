8296dbfefbfc0eb03568a4fc02a39dfc57dd4fd4
Brandooo! <becruz1738@gmail.com>
2023-07-16 21:39:06 -0500

    game

--- src/lib/GameStation/Games.svelte
+++ src/lib/GameStation/Games.svelte
@@ -3,8 +3,10 @@
 
     EJS_player = '#game';
     EJS_biosUrl = '/scph1001.bin'; // Url to Bios file
-    EJS_gameUrl = ''; // Url to Game rom
+    EJS_gameUrl = 'https://drive.google.com/u/0/nonceSigner?nonce=63pqs3m4ql246&continue=https://doc-00-6g-docs.googleusercontent.com/docs/securesc/4789avb5pb4p6s2gtvm7hgf4g1q8j0kv/eq4ihtu0qk6j99bag3v3jghjo4r65f15/1689561450000/13623365757509044785/13623365757509044785/1hUKuwGDYxrmc5aYLhlSj9PGJ1mSicCIe?e%3Ddownload%26ax%3DAGtFMPVbMVMVzbKho9pHOetOMuO2aqqVXRAaX-nCKAZ4QggZiN8k7ANWf63J9LUkxiSogidqSGbGfqxVK4GB9puTigHBxu83c1pEi9HcZ6jKc4-umlmz0mkRnadnqTnHzTgIP79zyKkfpJHE-jmJOg6bDvyA2JYZZ9UTgaU0uGppJNyovqlNfdnwBs280N1qpqc7xdOpp2qOgOCmjeoGL3WOWtN1llZMdQfAhfHZ_97LyBhR5Wmfyv5z__jhQC7T8lGDC0foilsMyoFfawMUIOb-2xvnSE1hafZ6B8-cdeELNvZlWtjXHY2XrOmizPyiEaIE-RW-fqFSVGezLqObFeevsiIsZsGxOjACdVS76YkQEltRdD_cu6gX_lvocj96gi_rU4CKNiSYgxk_LZPZGILdxOyoUJ1V6usRLcjQF1GXCaLc2l1rii_uUyfDF4g8nMrmh7kMEdnl3GmOVwH9L7ijPXWuxMyB0bT3BltHOZ6VVxq8k9Y67_nglemFl5auJDCEMm-jjs1rTeTtR0HYmE57y284aoxdJjW7QE9mvEgnFsGHY5ANrCFGkQdrTiupzSBW2we0xGM9-LI_w9Zy1pxwHMi0rhn3Go_EEviNa9HJLT82I7wjVZKugtGpEGOS4jik6ifCKemTUvN26MpWpGGLiVD1LqOrcbZyaA3BrgIAlMeuuLdM_AJ1rKg9VNZwO9WDhBSKJEpJz_EuOb6Gw389momPXQE4ayqG4_VLZ1TzVFbcxI__x30xyCtUbaPnX0PSMnGHr3obx6xsQzNi7iBs1jy5eXxn8Ax8Pn9x0UIHB7A7G37w1mcXDa-Y9nZ6jaHE1qX3_dBksumEZ0A1Sg-HYrafgtoO8R5DBQDstpNjjrR0dhkjyTUXQ_dz1heXpxfuJ5cBrYGfc-TN12rQd4lapm2tsdZUhNkRFUKnpig%26uuid%3D71348fec-d4cc-43d9-af52-37b70a51feba%26authuser%3D0&hash=q2s14v6ue6op53cd408kqmo14i1de2v7'; // Url to Game rom
     EJS_core = 'psx';
+
+    // https://drive.google.com/uc?export=download&id=1hUKuwGDYxrmc5aYLhlSj9PGJ1mSicCIe&confirm=t&uuid=71348fec-d4cc-43d9-af52-37b70a51feba
 </script>
 
 <div class="container" id="emuPanel">
