c2941ed8261948bd911fef5f2ec84ad65067caa6
Brandooo! <82909107+cptnbrando@users.noreply.github.com>
2023-08-09 23:24:01 -0500

    added button for radio

---
+++ src/lib/radio/SpotifyChecker.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+    const spotifyClientID: string = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+    const spotifySecret: string = import.meta.env.VITE_SPOTIFY_SECRET;
+
+    let live: boolean = false;
+</script>
+
+<div>
+    {#if live}
+        <h3>Captain Brando is online!</h3>
+    {:else}
+        <h3>Captain Brando is offline...</h3>
+    {/if}
+</div>
+
+<style>
+    div {
+        border: solid;
+        margin-top: 5%;
+        padding-left: 1%;
+        padding-right: 1%;
+    }
+</style>
