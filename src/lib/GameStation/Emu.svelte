9bed01f226f448a8360b476468733365fd2a7c34
Brando <82909107+cptnbrando@users.noreply.github.com>
2025-01-20 22:51:32 -0600

    gametime

--- src/lib/GameStation/Emu.svelte
+++ src/lib/GameStation/Emu.svelte
@@ -1,50 +1,8 @@
 <script lang="ts">
-    // This needs to be in the index.html initial file for svelte
 
-    // Platform select
-    var NepEmu = "n64";
-    // ID of html element where the emulator will be inserted
-    var NepPlayer = "#emulator";
-
-    // var neogeoBios = "./test/bios/neogeo.zip";
-    // Use default psxBios included with NeptunJS, as the psx bios files make it slower
-    // var psxBios = "./test/bios/psxBios.zip";
-    var psxSkipBios = false;
-
-    // Add extra button on toolbar to scale up emulator. Disabled by default
-    var NepZoom = "enable";
-    // Maximum width size of emulator. 870px by default
-    var NepMaxWidth = "1100px";
-    // Interface language of the emulator. EN by default
-    var NepLang = "en";
-    // ( only for GBA ) GBA Bios Need for some games. Disabled by default
-    var EmuGbaBios = "enable";
+    // This needs to be here for some reason. Something about styles, idk, don't touch it
+    // Go to the root index \captainbrando-svelte\index.html to change what you need to change for games
     
-    function getGame() {
-        let game = `/neptun/test/`;
-        switch(NepEmu) {
-            case "n64":
-                game += "1m.zip";
-                break;
-            case "gba":
-                game += "2p.zip";
-                break;
-            case "psx":
-                game += "3s.zip";
-                break;
-            default:
-                // Default to the first game
-                game += "1m.zip";
-                break;
-        }
-
-        return game;
-    }
-
-    // Game Url
-    // var gameUrl = getGame();
-    var gameUrl = `/neptun/test/1m.zip`;
-
     var scriptsus = document.createElement("script");
     scriptsus.setAttribute("data-name", "njs2");
 
