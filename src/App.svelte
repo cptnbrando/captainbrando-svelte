810b2b4a20c38db156c4bdda525b960efcc9cf4f
Brando <82909107+cptnbrando@users.noreply.github.com>
2025-09-09 09:48:35 -0500

    Transparency

--- src/App.svelte
+++ src/App.svelte
@@ -73,6 +73,7 @@
 		-moz-user-select: none; /* Old versions of Firefox */
 		-ms-user-select: none; /* Internet Explorer/Edge */
 		user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
+		-webkit-tap-highlight-color: transparent;
 	}
 
 	article {
